a82dc490dee0a59627111a068a9d80faa1d0417a
Princeyadav1611 <princeyadav161101@gmail.com>
2026-07-13 08:07:43 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void)
+{
+    int A;
+    int B;
+    printf("Enter a number ");
+    scanf("%d", &A);
+    printf("Enter one more  number ");
+    scanf("%d", &B);
+    
+     int sum = A + B;
+    printf("Your sum is %d\n", sum);
+    
+/*  yha pe jo hmne  int sum = A + B;
+    printf("Your sum is %d\n", sum); kiya hai iske badle hm direct easy kr skte hai jo ki hai ki 
+     int sum = A + B; isko hata do and ise badle liko 
+      printf("Your sum is %d\n", A = B);
+      this will be good 
+
+
+
+*/
+
+
+
+}
---
+++ variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+// ye saare ke ssaare inputs hai diff types ke 
+// a b cd ya sp characters hmesha 'a' 'b' format me rhenge
+// real integers is 1.0 , 1.33 etc krke 
+// to print int %d
+// to print real integers %f
+// to print characters %c
+
+
+    int number = 22;
+    char star = '#';
+    float pi = 3.14;
+    int age = 42;
+
+    int a = 22;
+    int A = 25;
+    int _price = 300;
+
+    printf("number is %d\n", age);
+// thus is to print variables , variable ka name likhna ki kis name se saved hai 
+      return 0;
+}
