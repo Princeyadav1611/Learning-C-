2068fd5a5ac236d2909b3088dcd7e1046dbc50b2
Princeyadav1611 <princeyadav161101@gmail.com>
2026-07-13 08:10:17 +0530

    Add files via upload

---
+++ circle_area.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+
+    float a;
+    printf("What is the radius of the circle ?");
+    scanf("%f", &a);
+    printf("The area of the circle is %f", a * a * 3.14);
+/*
+Tum chaho to shuru me iinteger rkh skte ho but last me float hi rkhna yunki 3.14 yani ki
+ pi ek integer hai to isliye .
+*/
+}
---
+++ sqaure_area.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    printf("What is the side of the sqaure ?");
+    scanf("%d", &a);
+    
+   printf("Area of the sqaure is %d", a * a);
+}
